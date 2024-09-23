6ece2c1ea893a82610ee7d60a36ca1f1784db53a
nemmondommeg <nemmondommeg>
2024-09-23 11:14:43 +0200

    Created database connection

---
+++ Connection.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoConsole
+{
+    internal class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connection()
+        {
+            Host = "127.0.0.1";
+            Database = "Cars";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+            
+            connection = new MySqlConnection(ConnectionString);
+        }
+    }
+
+
+}
